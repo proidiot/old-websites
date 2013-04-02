dd7b7bcb8ed6d677cb0ce0e84d4fb133b7713fea
Charles Southerland <charlie@stuphlabs.com>
2013-04-02 04:17:17 -0500

    Added signal trapping to replace the ugly old getchar that used to control program termination.

--- doc/tutorial-ch7/main.c
+++ doc/tutorial-ch7/main.c
@@ -2,14 +2,31 @@
 #include "answer.h"
 #include "request_cleanup.h"
 #include "daemon_state.h"
+#include "debug.h"
+#include <string.h>
+#include <signal.h>
+#include <stdlib.h>
 
 #define MAX_CLIENTS 50
 #define MAX_UPLOAD_CLIENTS 2
 #define UPLOAD_MULTIPLIER 24
 
+void catch(int signal)
+{
+	debug("Caught signal %d: %s", signal, strsignal(signal));
+}
+
 int main()
 {
+	sigset_t empty_sigset;
 	struct MHD_Daemon* daemon;
+
+	signal(SIGHUP, SIG_IGN);
+	signal(SIGINT, &catch);
+	signal(SIGTERM, &catch);
+	sigemptyset(&empty_sigset);
+
+	debug("Starting daemon...");
 	DAEMON_STATE dstate = new_DAEMON_STATE(
 			MAX_CLIENTS,
 			MAX_UPLOAD_CLIENTS,
@@ -25,14 +42,22 @@ int main()
 		&request_cleanup,
 		(void*)dstate,
 		MHD_OPTION_END);
+
 	if (daemon == NULL) {
-		return 1;
+		debug("Unable to start daemon.");
+		destroy_DAEMON_STATE(dstate);
+		return EXIT_FAILURE;
+	} else {
+		debug("Daemon has been started.");
+
+		sigsuspend(&empty_sigset);
+	
+		debug("Stopping daemon...");
+		MHD_stop_daemon(daemon);
+		destroy_DAEMON_STATE(dstate);
+
+		debug("Daemon has been stopped.");
+		return EXIT_SUCCESS;
 	}
-
-	getchar();
-
-	MHD_stop_daemon(daemon);
-	destroy_DAEMON_STATE(dstate);
-	return 0;
 }
 
